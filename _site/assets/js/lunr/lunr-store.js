4de89a1cba20eaacb64afc094d83096fe73503e0
조을연 <carpinus0@gmail.com>
2020-06-01 22:09:30 +0900

    Add mark down test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Markdown Test",
+        "excerpt":"Markdown 에디터로서의 노션 테스트 마크다운에디터로서 노션이 얼마나 쓸만한지 테스트해보는 포스팅입니다. 현재 이 포스팅은 모두 노션으로 쓰여졌습니다. 이러한 리스트들도 마크다운문법에 맞게 -를 타이핑하면 자동으로 생성이됩니다. 훨씬 보기는 쉬운데 이게 Export가 잘 될지는 미지수입니다. 일단 써보고는 있습니다. 번호로 적는 리스트도 잘되는군요 라이브로 보인다는게 강점이긴합니다. print(\"Code Snippet Test\") print(\"python Code역시 잘됩니다\") 라인도 잘되는군요....","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/markdown-test/",
+        "teaser": null
+      },{
         "title": "github 블로그 첫 포스팅",
         "excerpt":"깃허브블로그에 jekyll을 활용한 블로그 포스팅 테스트입니다. 마크다운문법을 활용해 좀 더 정돈된 글을 쓸수있습니다. 일단 테스트삼아 올려봅니다.   익숙해지는데 조금 시간이 걸리겠지만, 한번 사용해보기로!!   코드블록은 요렇게 사용한다.      print(\"Hello Github Blog\")      마크다운에 익숙해져야 편하게 쓰긴하겠네요! 존크루버 마크다운번역페이지를 참고해서 공부가 필요!  ","categories": ["jekyll","update"],
         "tags": [],
